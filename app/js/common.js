a4aa233ed31421ed42d8a919f9a03a74dc1c666d
Tochoniy Alexey <tochoniy_alexey@ukr.net>
2018-03-27 10:35:55 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,152 @@
+$(function() {
+
+	var owl = $(".slider");
+	owl.owlCarousel({
+		 loop: true,
+		 items: 1,
+		 itemClass: "slide-wrap",
+		 nav: true,
+		 navText: ""
+	});
+	$(".next").click(function(){
+		owl.trigger('next.owl.carousel');
+	})
+	$(".prev").click(function(){
+		owl.trigger('prev.owl.carousel');
+	});
+
+	
+
+	function heightses() {
+		
+		$(".offer-text h4").height('auto').equalHeights();
+		$(".offer-btn").height('auto').equalHeights();
+		$(".footer_item").height('auto').equalHeights();
+	}
+
+	$(window).resize(function() {
+		heightses();
+	});
+
+	heightses();
+
+	
+	
+
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(this).parent().next().next().find(".main-mnu").slideToggle();
+		return false;
+	});
+
+
+
+
+
+	$(".portfolio-item").each(function(e) {
+
+		var th = $(this);
+
+		th.attr("href", "#portfolio-img-" + e)
+			.find(".portfolio-popup")
+				.attr("id", "portfolio-img-" + e);
+
+	});
+
+
+
+	$(".portfolio-item, a[href='#callback']").magnificPopup({
+		mainClass: 'my-mfp-zoom-in',
+		removalDelay: 300,
+		type: 'inline',
+	});
+
+
+	$(".mfp-gallery").each(function() {
+		$(this).magnificPopup({
+			delegate: 'a',
+			mainClass: 'mfp-zoom-in',
+			type: 'image',
+			tLoading: '',
+			gallery:{
+				enabled: true,
+			},
+			removalDelay: 300,
+			callbacks: {
+				beforeChange: function() {
+					this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+				},
+				open: function() {
+					$.magnificPopup.instance.next = function() {
+						var self = this;
+						self.wrap.removeClass('mfp-image-loaded');
+						setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+					}
+					$.magnificPopup.instance.prev = function() {
+						var self = this;
+						self.wrap.removeClass('mfp-image-loaded');
+						setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+					}
+				},
+				imageLoadComplete: function() { 
+					var self = this;
+					setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+				}
+			}
+		});
+	});
+
+
+	
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	
+	
+
+
+	$("body").on("click", ".top", function() {
+		$("html, body").animate({scrollTop: 0}, "slow");
+	});
+	$("body").append('<div class="top"><i class="fa fa-angle-double-up">');
+
+	$(window).scroll(function() {
+		if($(this).scrollTop() > $(this).height()) {
+			$(".top").addClass("active");
+		} else {
+			$(".top").removeClass("active");
+		}
+	});
+	
+
+
+
+		//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
